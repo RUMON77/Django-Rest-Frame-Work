fbba854521521e2f5e965069766d93c1343e2d6c
AL Shariar <65387685+RUMON77@users.noreply.github.com>
2021-07-07 20:47:14 +0600

    Update urls.py

--- Django/rest_frame/restapp/urls.py
+++ Django/rest_frame/restapp/urls.py
@@ -1,18 +1,32 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from restapp.views import GenericArticleView
+#from restapp.views import GenericArticleView
+from rest_framework.routers import DefaultRouter
+#router = DefaultRouter()
+#router.register(r'article', GenericArticleView, basename='article')
+
+
+from restapp.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+
+
 
 urlpatterns = [
     path('article/', views.ArticleList.as_view()),
     path('article/<int:pk>/', views.ArticleDetail.as_view()),
-    path('generic/article/', GenericArticleView.as_view(), name="genericarticle"),
-    path('generic/article/<int:id>/', GenericArticleView.as_view(), name="genericarticle"),
+    #path('generic/article/', GenericArticleView.as_view(), name="genericarticle"),
+    #path('generic/article/<int:id>/', GenericArticleView.as_view(), name="genericarticle"),
+    path('viewsets/', include(router.urls)),
+    path('viewsets/<int:pk>/', include(router.urls)),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
 
 """
 if settings.DEBUG:
